17f39fbe2f779a9d85960f75bef4b17a68923edc
Pratik Behera <tamylv.pb@gmail.com>
2023-04-02 19:58:24 +0530

    Added stock-price-app

---
+++ Stock-Price-App/main.py
@@ -0,0 +1,32 @@
+from datetime import datetime 
+import yfinance as yf
+import streamlit as st  
+import pandas as pd
+
+
+st.write("""
+# Stock Price App
+
+Shown are the **stock closing price** and **volume** of Google 
+
+""")
+
+amzn = yf.Ticker("AMZN")
+end_date = datetime.now().strftime('%Y-%m-%d')
+print(end_date)
+amzn_hist = amzn.history(start='2023-01-01',end=end_date)
+print(amzn_hist)
+
+st.write("""
+# **Closing Price**
+
+""")
+st.line_chart(amzn_hist.Close)
+st.write("""
+# **Volume**
+
+""")
+st.line_chart(amzn_hist.Volume)
+
+
+
